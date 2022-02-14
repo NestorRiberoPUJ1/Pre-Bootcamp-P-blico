882b7e7da73d70ba5ea444e5dc07e85bb5cb6b59
Francisco Carlos Calderon <calderonf@gmail.com>
2022-02-14 16:09:39 -0500

    Avances

---
+++ RIBERO_NESTOR_JSPrediccion.js
@@ -0,0 +1,16 @@
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}   //Nací en 1980
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+}   //Nací en 1980
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}   //¡Sumando números!
+    //num1 is: 10
+    //num2 is: 20
+    //30
